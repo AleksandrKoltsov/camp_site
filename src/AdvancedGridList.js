4838858e2bc375965d2c19a2aded91352b629d9e
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-03-24 19:52:25 +0200

    Add GridList component for img render

---
+++ src/AdvancedGridList.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
+const useStyles = makeStyles(theme => ({
+  gridList: {
+    width: "100%",
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+  icon: {
+    color: 'white',
+  },
+}));
+
+export default function AdvancedGridList(props) {
+  const classes = useStyles();
+console.log(props.data);
+props.data.featured? console.log('true'):console.log("false");
+  
+return (
+  <div>
+      <GridList cellHeight={200} spacing={1} className={classes.gridList}>
+        {props.data.map(tile => (
+          <GridListTile key={tile.img} cols={1} rows={2}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              titlePosition="top"
+              actionIcon={
+                <IconButton aria-label={`star ${tile.title}`} className={classes.icon}>
+                  <StarBorderIcon />
+                </IconButton>
+              }
+              actionPosition="left"
+              className={classes.titleBar}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
+
+
