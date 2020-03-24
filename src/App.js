79b1409103762526cd1a61fc3b69c14982e85dea
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-03-24 19:53:57 +0200

    Add CardsFactory as props to Drawer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PersistentDrawerLeft from './Menu.js';
 // import logo from './logo.svg';
 import './App.css';
+import CardsFactory from './CardsFactory.js';
 
 function App() {
   return (
-   <div><PersistentDrawerLeft /></div>
+   <div><PersistentDrawerLeft cards={<CardsFactory/>}/></div>
   );
 }
 
