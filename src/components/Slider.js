15f880529b7e034c800b2cada339de1f0ed04f7e
AleksandrKoltsov <41367097+AleksandrKoltsov@users.noreply.github.com>
2020-03-27 15:45:45 +0200

    merge branches

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 
