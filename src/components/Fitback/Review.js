e6aae8d4887f9bfb70581981db5687ea31cc34b3
AleksandrKoltsov <41367097+AleksandrKoltsov@users.noreply.github.com>
2020-06-02 11:19:44 +0300

    fitback component completed

--- src/components/Fitback/Review.js
+++ src/components/Fitback/Review.js
@@ -3,8 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import FullStar from "./FullStar";
-import EmptyStar from "./EmptyStar";
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import StarIcon from '@material-ui/icons/Star';
+
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -19,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
         fontSize: '1.2rem'
     },
     star: {
-        margin: '0 0 0.5rem 0'
+        margin: '0 0 0 0'
     },
     text: {
         textIndent: '1.5rem',
@@ -32,38 +33,23 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Review (props) {
-    // console.log(props);
-    // console.log(fullStar, emptyStar);
     const classes = useStyles();
 
     const renderStar = (rat) => {
-
-        let str = Array(10);
-        for(let i = 10, star = rat; i < 10; i++){
+        console.log(rat);
+        let str = [];
+        for(let i = 1, star = rat; i < 10; i++){
 
             if(star > 0) {
-                str.push(<FullStar key={i}/>);
+                str.push(<span key={i}><StarIcon color='primary' fontSize='small' /></span>);
                 star--;
-                console.log(str);
             }else {
-                str.push(<EmptyStar key={i}/>);
+                str.push(<span key={i}><StarBorderIcon color='primary' fontSize='small' /></span>);
             }
         }
+        console.log(str);
         return str;
     };
-
-    // const formatDate = () => {
-    //     let date = new Date();
-    //     let dd = date.getDate();
-    //     if (dd < 10) dd = "0" + dd;
-    //
-    //     let mm = date.getMonth() + 1;
-    //     if (mm < 10) mm = "0" + mm;
-    //
-    //     let yyyy = date.getFullYear();
-    //
-    //     return dd + "." + mm + "." + yyyy;
-    // };
 
     return (
         <Box
@@ -72,7 +58,7 @@ export default function Review (props) {
         >
             <CssBaseline />
                 <span className={classes.name}>{props.item.name}</span>
-                <div className={classes.star}>{renderStar(props.item.rating)}</div>
+                <span className={classes.star}>{renderStar(props.item.rating)}</span>
                 <span className={classes.date}>{props.item.date}</span>
                 <span className={classes.text}>{props.item.review}</span>
         </Box>
