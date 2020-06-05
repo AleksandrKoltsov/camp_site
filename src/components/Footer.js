ec95aaff424eacb962e18fecaa87d891fa4539f8
AleksandrKoltsov <41367097+AleksandrKoltsov@users.noreply.github.com>
2020-06-05 21:09:53 +0300

    work with style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,10 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TelegramIcon from '@material-ui/icons/Telegram';
 
 function Copyright() {
     return (
@@ -22,7 +26,6 @@ const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
         flexDirection: 'column',
-        // minHeight: '100vh',
     },
     main: {
         marginTop: theme.spacing(8),
@@ -32,9 +35,43 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(3, 2),
         marginTop: 'auto',
         minHeight: '30vh',
-        backgroundColor: '#3f51b5',
-        color: 'white'
-            // theme.palette.type === 'dark' ? theme.palette.grey[200] : theme.palette.grey[800],
+        background: 'linear-gradient(#1565c0, #0d47a1, #212121);',
+        color: 'white',
+        boxShadow: '0 -10px 18px rgba(0,0,0,0.4), 0 10px 10px rgba(0,0,0,0.3)'
+    },
+    container: {
+        display: 'flex',
+    },
+    header: {
+        display: 'block',
+        fontWeight: '600',
+        color: '#fff',
+        margin: '0, 2rem, 0, 2rem',
+        border: '1px solid red',
+        textAlign: 'center'
+    },
+    smallCont: {
+        display: 'block',
+        justifyContent: 'center',
+        margin: '0 1rem 0 1rem',
+        width: '20vw',
+    },
+    text: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        listStyleType: 'none',
+        fontSize: '1rem',
+        border: '1px solid red',
+        '& a, a:link, a:visited, a:active': {
+            color: 'rgba(255,255,255, 0.7)',
+            textDecoration: 'none',
+            fontWeight: '100'
+        },
+        '& a:hover':{
+            color: 'rgba(255,255,255)',
+            textDecoration: 'underline',
+        }
     },
 }));
 
@@ -44,16 +81,43 @@ export default function StickyFooter() {
     return (
         <div className={classes.root}>
             <CssBaseline />
-        {/*    <Container component="main" className={classes.main} maxWidth="sm">*/}
-        {/*    <Typography variant="h2" component="h1" gutterBottom>*/}
-        {/*        Sticky footer*/}
-        {/*    </Typography>*/}
-        {/*    <Typography variant="body1">Sticky footer placeholder.</Typography>*/}
-        {/*</Container>*/}
             <footer className={classes.footer}>
                 <Container maxWidth="sm">
-                    <Typography variant="body1">My sticky footer can be found here.</Typography>
-                    <Copyright />
+                    <Grid container spacing={2}>
+                        <Grid item>
+                            <Typography>
+                                <span className={classes.header}>Проект</span>
+                                <ul className={classes.text}>
+                                    <li><a href="#">GitHub</a></li>
+                                    <li><a href="#">Heroku</a></li>
+                                    <li><a href="#">Google</a></li>
+                                </ul>
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography>
+                                <span className={classes.header}>Підтримка</span>
+                                <ul className={classes.text}>
+                                    <li><a href="#">Зауваження</a></li>
+                                    <li><a href="#">Питання</a></li>
+                                    <li><a href="#">Сповістити про помилку</a></li>
+                                    <li><a href="#">Отримати допомогу</a></li>
+                                </ul>
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography>
+                            <div className={classes.text}>
+                                <FacebookIcon />
+                                <InstagramIcon />
+                                <TelegramIcon />
+                            </div>
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Copyright />
+                        </Grid>
+                    </Grid>
                 </Container>
             </footer>
         </div>
