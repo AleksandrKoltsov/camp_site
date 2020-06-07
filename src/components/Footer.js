2383e6955452bb8a6f2e872c16497f33f66bd84f
AleksandrKoltsov <41367097+AleksandrKoltsov@users.noreply.github.com>
2020-06-07 11:34:57 +0300

    layout completed, left to fill links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,7 +25,7 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
     },
     main: {
         marginTop: theme.spacing(8),
@@ -35,43 +35,68 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(3, 2),
         marginTop: 'auto',
         minHeight: '30vh',
-        background: 'linear-gradient(#1565c0, #0d47a1, #212121);',
+        // backgroundColor: '#0d47a1',
+        background: 'linear-gradient(#3241A2, #3241A2, #212121 100%);',
         color: 'white',
-        boxShadow: '0 -10px 18px rgba(0,0,0,0.4), 0 10px 10px rgba(0,0,0,0.3)'
-    },
-    container: {
-        display: 'flex',
-    },
-    header: {
-        display: 'block',
-        fontWeight: '600',
-        color: '#fff',
-        margin: '0, 2rem, 0, 2rem',
-        border: '1px solid red',
-        textAlign: 'center'
-    },
-    smallCont: {
-        display: 'block',
-        justifyContent: 'center',
-        margin: '0 1rem 0 1rem',
-        width: '20vw',
-    },
-    text: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        listStyleType: 'none',
-        fontSize: '1rem',
-        border: '1px solid red',
+        boxShadow: '0 -10px 18px rgba(0,0,0,0.4), 0 10px 10px rgba(0,0,0,0.3)',
         '& a, a:link, a:visited, a:active': {
             color: 'rgba(255,255,255, 0.7)',
             textDecoration: 'none',
-            fontWeight: '100'
+            fontWeight: '100',
         },
         '& a:hover':{
             color: 'rgba(255,255,255)',
             textDecoration: 'underline',
         }
+    },
+    container: {
+        boxSizing: 'borderbox',
+        display: 'flex',
+        flexFlow: 'row',
+        justifyContent: 'space-around',
+        alignContent: 'flex-start',
+        width: '100%',
+    },
+    smallCont: {
+        // margin: '1rem',
+        padding: '0.8rem',
+        flex: '0 1 auto',
+        flexDirection: 'column',
+        alignItems: 'center',
+        // alignSelf: 'stretch',
+        '& smallCont:nth-child(1)': {
+            flex: '0 1 30vw',
+        },
+        '& smallCont:nth-child(2)': {
+            flex: '0 1 30vw',
+        },
+        '& smallCont:nth-child(3)': {
+            flex: '0 1 30vw',
+        },
+    },
+    header: {
+        display: 'block',
+        fontSize: '1.1rem',
+        fontWeight: '600',
+        color: '#fff',
+        margin: '0, 2rem, 0, 2rem',
+        padding: '0 0 0 2.5rem',
+        // border: '1px solid red',
+        // textAlign: 'center'
+    },
+    text: {
+        listStyleType: 'none',
+        fontSize: '1rem',
+        // border: '1px solid red',
+    },
+    copyright: {
+        display: 'block',
+        textAlign: 'center',
+        width: '96.5vw',
+        // border: '1px solid red',
+    },
+    soc: {
+        padding: '2rem',
     },
 }));
 
@@ -82,43 +107,35 @@ export default function StickyFooter() {
         <div className={classes.root}>
             <CssBaseline />
             <footer className={classes.footer}>
-                <Container maxWidth="sm">
-                    <Grid container spacing={2}>
-                        <Grid item>
-                            <Typography>
-                                <span className={classes.header}>Проект</span>
-                                <ul className={classes.text}>
-                                    <li><a href="#">GitHub</a></li>
-                                    <li><a href="#">Heroku</a></li>
-                                    <li><a href="#">Google</a></li>
-                                </ul>
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography>
-                                <span className={classes.header}>Підтримка</span>
-                                <ul className={classes.text}>
-                                    <li><a href="#">Зауваження</a></li>
-                                    <li><a href="#">Питання</a></li>
-                                    <li><a href="#">Сповістити про помилку</a></li>
-                                    <li><a href="#">Отримати допомогу</a></li>
-                                </ul>
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography>
-                            <div className={classes.text}>
-                                <FacebookIcon />
-                                <InstagramIcon />
-                                <TelegramIcon />
-                            </div>
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <Copyright />
-                        </Grid>
+                <Grid container spacing={2} className={classes.container}>
+                    <Grid container className={classes.smallCont}>
+                        <span className={classes.header}>Проект</span>
+                        <ul className={classes.text}>
+                            <li><a href="#">GitHub</a></li>
+                            <li><a href="#">Heroku</a></li>
+                            <li><a href="#">Google</a></li>
+                        </ul>
                     </Grid>
-                </Container>
+                    <Grid container className={classes.smallCont}>
+                        <span className={classes.header}>Підтримка</span>
+                        <ul className={classes.text}>
+                            <li><a href="#">Зауваження</a></li>
+                            <li><a href="#">Питання</a></li>
+                            <li><a href="#">Сповістити про помилку</a></li>
+                            <li><a href="#">Отримати допомогу</a></li>
+                        </ul>
+                    </Grid>
+                    <Grid container className={classes.smallCont}>
+                        <div className={classes.soc}>
+                            <a href=""><FacebookIcon  fontSize='large'/></a>
+                            <a href=""><InstagramIcon fontSize='large'/></a>
+                            <a href=""><TelegramIcon fontSize='large'/></a>
+                        </div>
+                    </Grid>
+                </Grid>
+                    <Grid item className={classes.copyright}>
+                        <Copyright />
+                    </Grid>
             </footer>
         </div>
     );
