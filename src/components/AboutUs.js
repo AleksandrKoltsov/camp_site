e61ada76a88b10b22779410bd81f855362e63dd3
AleksandrKoltsov <41367097+AleksandrKoltsov@users.noreply.github.com>
2020-06-10 20:03:06 +0300

    add info aboutus

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -13,6 +13,7 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
+        fontSize: '1rem'
     },
 }));
 
@@ -23,15 +24,43 @@ export default function AboutUs() {
         <Container component="main" maxWidth="lg">
             <CssBaseline />
             <div className={classes.paper}>
-                <h1>Про нас</h1>
-                <hr/>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque ipsa iste magni maxime, natus soluta veritatis. A accusantium ad asperiores debitis dicta dolorem, doloribus enim expedita illum, ipsam ipsum molestiae nesciunt non obcaecati odit quas quo. A ab accusamus ad aliquam aperiam autem commodi cupiditate deserunt eveniet ex exercitationem explicabo fugit id illum impedit molestias odio perspiciatis quae quasi quia quis quos rem reprehenderit repudiandae rerum, tempora temporibus ullam velit veniam voluptate? Incidunt obcaecati quisquam quos? Commodi cupiditate dolore dolores, dolorum eligendi fuga harum illo impedit ipsam iure minus, nesciunt nulla omnis optio placeat quasi qui reiciendis rem totam ut velit veritatis vero. Accusantium aut commodi consequuntur dolore explicabo itaque neque nulla quaerat totam voluptates! Amet autem cumque cupiditate, dicta dignissimos dolor earum error facere hic in iste labore libero magni minus nemo neque numquam pariatur perspiciatis possimus quidem repellat sapiente ullam vero. Dicta nobis odio quidem! Debitis deserunt et labore numquam provident quia sapiente, similique. Ad amet animi culpa cumque, debitis delectus dignissimos dolores eos error esse est eveniet expedita, harum illo in iste iure iusto laborum laudantium minus modi molestias, nesciunt nostrum nulla numquam obcaecati praesentium quibusdam quisquam ratione reprehenderit repudiandae saepe tenetur totam ullam unde veritatis voluptas.!
-                </p>
-                <img src="https://lh3.googleusercontent.com/proxy/_qviZ3i-aeA72alsNkDFWI9BnNXeSyCwJX_EaC6zwakRzFHNarKiBpXenSeD3I4Crpm7ebPGxEpGHpOGZaQQ1J0MzZBgoD8rxmJV5FOZwl3s" alt="дубрава"/>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci blanditiis delectus deserunt dignissimos distinctio ea ex iure libero nisi non optio placeat provident quam quisquam quos vero vitae, voluptas. A est et, id ipsum iste, laudantium maiores molestiae nihil numquam, perferendis quos tempore voluptatem! Alias beatae consequuntur dicta doloremque eveniet, exercitationem, perspiciatis, sed sequi totam velit voluptates voluptatum? Ad aut culpa eius exercitationem incidunt minima repellendus sit soluta voluptas. Ex, maiores, vero! Animi at deserunt expedita inventore ipsam nam porro velit. Harum, ipsam modi omnis provident quos voluptates? Autem beatae commodi cum delectus deleniti, eaque est incidunt ipsum maxime minima minus molestiae numquam perferendis porro provident quis reiciendis repudiandae saepe sequi sint, sunt temporibus tenetur ullam, vel vero? Accusamus ad adipisci alias asperiores aspernatur atque aut consectetur corporis cumque cupiditate debitis deserunt doloremque eligendi eum excepturi explicabo illo ipsa labore magni, necessitatibus odio officiis quis quisquam saepe sapiente sint sit suscipit tempora tenetur vero. A ab alias, amet assumenda cumque debitis dignissimos eius ex explicabo iste itaque labore magnam maiores modi nemo nobis nostrum placeat quasi qui quod quos ratione, repellat reprehenderit repudiandae sint ullam vero. Culpa deleniti eos excepturi expedita iste magnam modi pariatur perferendis, quidem vel veritatis voluptatibus?
-                </p>
+                    <span><b>Про нас:</b></span>
+                    <h3>Расположение:</h3>
+                    <p>База отдыха «Дубрава» ЮМЗ находится в Новомосковском районе Днепропетровской области, в селеХащевое</p>
+                    <h3>База отдыха «Дубрава» размещение:</h3>
+                <p>На территории базы отдыха Дубрава построены двухэтажные кирпичные домики:</p><br/>
+                        <strong>трехместные:</strong> санузел, душ или ванна;<br/>
+                        <strong>четырехместные:</strong> санузел, душ или ванна.<br/>
+                        Не все домики имеют подачу горячей воды и отопление. Возле домиков есть столы и мангалы.<br/>
+                        <p>Работает с мая по сентябрь.</p>
+                    <h3>База отдыха «Дубрава» инфраструктура :</h3>
+                    <ul>
+                        <li>сауна,</li>
+                        <li>футбольная площадка,</li>
+                        <li>два бадминтонных корта,</li>
+                        <li>баскетбольная площадка,</li>
+                        <li>стол для пинг-понга,</li>
+                        <li>спортивный комплекс,</li>
+                        <li>бассейн,</li>
+                        <li>пляж,</li>
+                        <li>летняя эстрада,</li>
+                        <li>видео-зал,</li>
+                        <li>массажный кабинет,</li>
+                        <li>стоматологический кабинет,</li>
+                        <li>паркинг.</li>
+                    </ul>
+                    <h3>База отдыха «Дубрава» дополнительные услуги:</h3>
+                    <ul>
+                        <li>прогулки на лодках,</li>
+                        <li>прогулки на катамаранах,</li>
+                        <li>прокат спортинвентаря,</li>
+                        <li>прокат пляжного инвентаря,</li>
+                        <li>прокат бытовой техники,</li>
+                        <li>рыбалка.</li>
+                    </ul>
+                    <p>Есть открытый бассейн, корты, спортплощадка, столовая. По территории дорожки асфальтированные,
+                        фонарики.</p>
+                        <p>База отдыха «Дубрава» работает с мая по сентябрь</p><br/>
             </div>
         </Container>
     );
